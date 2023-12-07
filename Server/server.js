b356bdbf9a0546cacaea751642b297fcaba6b227
Anurag Gupta <ag6526803@gmail.com>
2023-12-08 01:39:26 +0530

    database established

---
+++ Server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const cookieParser = require("cookie-parser");
+const database = require("./config/database");
+database.connect();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.send("Home Page");
+});
+
+app.listen(3000, ()=> {
+    console.log(`Server started at Port ${PORT}`);
+})
