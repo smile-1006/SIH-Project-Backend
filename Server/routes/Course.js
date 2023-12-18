4f732d5d9e351169c826dca54a7b5c594506ce2a
Anurag Gupta <85058448+anurag-int@users.noreply.github.com>
2023-12-19 03:03:57 +0530

    Update Course.js

--- Server/routes/Course.js
+++ Server/routes/Course.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { auth, isAdmin } = require("../middlewares/auth");
-const {   getAllCourses, create-course } = require("../controllers/Courses");
+const {   getAllCourses, create_course } = require("../controllers/Courses");
 // addCourse,
-router.post("/addCourse", auth, isAdmin, create-course);
+router.post("/addCourse", auth, isAdmin, create_course);
 router.get("/getAllCourses", getAllCourses);
 
 module.exports = router;
