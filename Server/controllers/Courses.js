a945d1cda99da1c1120375089d633c3fd0fd42f8
Smile <singhswapnil060303@gmail.com>
2023-12-19 00:41:14 +0530

    routes update

--- Server/controllers/Courses.js
+++ Server/controllers/Courses.js
@@ -1,41 +1,75 @@
 const Course = require("../models/Course");
 
+const ITEMS_PER_PAGE = 10;
 
-exports.addCourse = async(req, res) => {
+exports.getAllCourses = async(req, res) => {
     try{
-        const { course_name, instructor_name, platform, duration, start_in, course_status } = req.body;
+            let { page = 1 } = req.query; // Default to page 1 if not provided  
+            const search = req.query.search || "";
+            let sort = req.query.sort || "duration";
+            let platform = req.query.platform || "All";
+            console.log(platform); // Fix variable name
 
-        if(!course_name || !instructor_name || !platform || !duration || !start_in || !course_status){
+            const platformOption = [
+            "CEC",
+            "UGC",
+            "NITTTR",
+            "IIMB",
+            "AICTE",
+            "IGNOU",
+            "NPTEL",
+            "NIOS"
+            ];
+            console.log(platformOption);
+
+            // Fix variable name: change 'sector' to 'platform'
+            if (typeof req.query.platform === 'string') {
+            platform = req.query.platform.split(","); // Fix variable name
+            } else {
+            platform = [...platformOption];
+            }
+            req.query.sort ? (sort = req.query.sort.split(",")) : (sort = [sort]);
+
+            let sortBy = {};
+
+            // Correct variable names in sortBy logic
+            if (sort[0] === "duration") {
+            sortBy = { duration: 1 }; // Fix variable name
+            } else if (sort[0] === "course_status") {
+            sortBy = { aicte_id: 1 };
+            }
+            console.log(sortBy);
+
+            const totalCount = await Course.countDocuments();
+            const totalPages = Math.ceil(totalCount / ITEMS_PER_PAGE);
+
+            if (page < 1 || page > totalPages) {
             return res.status(400).json({
-                success : false,
-                message : "All Fields are Mandatory"
+                success: false,
+                message: "Invalid page number",
+            });
+            }
+
+            const skip = (page - 1) * ITEMS_PER_PAGE;
+
+            const allCourses = await Course.find({}, {course_name : true, instructor_name : true ,institute_name : true, platform : true, duration : true, start_in : true, course_status: true}).skip(skip).limit(ITEMS_PER_PAGE);
+            //const totalInstituteRegistered = allInstitute.length;
+
+            return res.status(200).json({
+                success : true,
+                //Registered_Institute : totalInstituteRegistered,
+                Total_Institute : totalCount,
+                Total_Pages : totalPages,
+                Current_Page : page,
+                data : allCourses
             })
-        }
-
-        //check the auth for AICTE-MEMBER or Admin
-        const existingID = await Course.findOne({ course_status});
-
-        if(existingID){
-            return res.status(400).json({
-                success : false,
-                message : "The Institute with this AICTE ID is already registered, please Put unique ID."
-            })
-        }
-
-        const newInstitution = await Course.create({
-            course_name, 
-            instructor_name, 
-            platform, 
-            duration, 
-            start_in, 
-            course_status
-        });
-
-        res.status(200).json({
-			success: true,
-			message: "Course Added Successfully",
-            data : newInstitution
+    }
+    catch(error){
+        console.log(error);
+		return res.status(404).json({
+			success: false,
+			message: `Can't Fetch Institute Data`,
+			error: error.message,
 		});
-
-
+	}
 }
